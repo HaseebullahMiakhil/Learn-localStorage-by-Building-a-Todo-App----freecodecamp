851c0a6626a85c757a844ba4eaf606de4915d413
HaseebullahMiakhil <tw773029@gmail.com>
2024-03-31 14:32:50 +0430

    js

--- main.js
+++ main.js
@@ -9,3 +9,15 @@
 // }
 // outer();
 
+function outer(){
+    let counter = 0;
+    function inner(){
+        counter++
+        console.log(counter);
+    }
+    return inner
+}
+const fn = outer();
+fn()
+fn()
+fn()
