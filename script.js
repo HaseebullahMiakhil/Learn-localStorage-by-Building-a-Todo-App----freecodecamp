bc549dd5658c4d4ab2c6a688954f7a300e544796
HaseebullahMiakhil <tw773029@gmail.com>
2024-03-31 15:23:06 +0430

    js 3

--- script.js
+++ script.js
@@ -33,10 +33,8 @@ taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-const taskObj = {
-  id: `${titleInput.value.toLowerCase().split(' ')}`.join('-'),
-  title: titleInput,
-  description: descriptionInput,
-  dueDate: dueDateInput
-};
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    
+  };
 });
